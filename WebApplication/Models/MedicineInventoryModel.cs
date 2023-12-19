7464ac02dad4d1a08c0ab45effca7958c4ee8aaa
21120421 <nguyenchicuongk21@gmail.com>
2023-12-20 02:36:49 +0700

    fixx

---
+++ WebApplication/Models/MedicineInventoryModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+	public class MedicineInventoryModel
+	{
+		public int Id { get; set; }
+		[Required]
+		public int MedicineId { get; set; }
+		[Required]
+		public DateOnly ExpiryDate { get; set; }
+		[Required]
+		public int InventoryQuantity { get; set; }
+		[Required]
+		public string? Unit { get; set; }
+	}
+}
--- WebApplication/Program.cs
+++ WebApplication/Program.cs
@@ -8,74 +8,75 @@ using WebApplication.Congif;
 
 namespace WebApplication
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddControllersWithViews();
+			// Add services to the container.
+			builder.Services.AddControllersWithViews();
 
-            builder.Services.AddDbContext<AppDbContext>(options =>
-            {
-                options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"),
-                    x => x.UseDateOnlyTimeOnly());
-            });
+			builder.Services.AddDbContext<AppDbContext>(options =>
+			{
+				options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"),
+					x => x.UseDateOnlyTimeOnly());
+			});
 
-            builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
-            {
-                options.Password.RequireNonAlphanumeric = false;
-                options.Password.RequireLowercase = false;
-                options.Password.RequireUppercase = false;
-                options.Password.RequiredLength = 2;
-            })
-            .AddEntityFrameworkStores<AppDbContext>();
+			builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+			{
+				options.Password.RequireNonAlphanumeric = false;
+				options.Password.RequireLowercase = false;
+				options.Password.RequireUppercase = false;
+				options.Password.RequiredLength = 2;
+			})
+			.AddEntityFrameworkStores<AppDbContext>();
 
-            builder.Services.ConfigureApplicationCookie(options =>
-            {
-                options.LoginPath = "/Account/Login";
-                options.AccessDeniedPath = "/Account/AccessDenied";
-            });
+			builder.Services.ConfigureApplicationCookie(options =>
+			{
+				options.LoginPath = "/Account/Login";
+				options.AccessDeniedPath = "/Account/AccessDenied";
+			});
 
-            builder.Services.AddScoped<CustomerRepository>();
-            builder.Services.AddScoped<DentistRepository>();
-            builder.Services.AddScoped<EmployeeRepository>();
-            builder.Services.AddScoped<AppointmentScheduleRepository>();
-            builder.Services.AddScoped<MedicineRepository>();
+			builder.Services.AddScoped<CustomerRepository>();
+			builder.Services.AddScoped<DentistRepository>();
+			builder.Services.AddScoped<EmployeeRepository>();
+			builder.Services.AddScoped<AppointmentScheduleRepository>();
+			builder.Services.AddScoped<MedicineRepository>();
+			builder.Services.AddScoped<MedicineInventoryRepository>();
 
-            // Auto Mapper Configurations
-            var mapperConfig = new MapperConfiguration(mc =>
-            {
-                mc.AddProfile(new ConfigAutoMapper());
-            });
+			// Auto Mapper Configurations
+			var mapperConfig = new MapperConfiguration(mc =>
+			{
+				mc.AddProfile(new ConfigAutoMapper());
+			});
 
-            IMapper mapper = mapperConfig.CreateMapper();
-            builder.Services.AddSingleton(mapper);
-            builder.Services.AddMvc();
+			IMapper mapper = mapperConfig.CreateMapper();
+			builder.Services.AddSingleton(mapper);
+			builder.Services.AddMvc();
 
-            var app = builder.Build();
+			var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
+			// Configure the HTTP request pipeline.
+			if (!app.Environment.IsDevelopment())
+			{
+				app.UseExceptionHandler("/Home/Error");
+				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+				app.UseHsts();
+			}
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
+			app.UseHttpsRedirection();
+			app.UseStaticFiles();
 
-            app.UseRouting();
+			app.UseRouting();
 
-            app.UseAuthorization();
+			app.UseAuthorization();
 
-            app.MapControllerRoute(
-                name: "default",
-                pattern: "{controller=Home}/{action=Index}/{id?}");
+			app.MapControllerRoute(
+				name: "default",
+				pattern: "{controller=Home}/{action=Index}/{id?}");
 
-            app.Run();
-        }
-    }
+			app.Run();
+		}
+	}
 }
