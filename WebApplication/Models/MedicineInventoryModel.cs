1b23bbb512b65c8a72b2e52b82c9199d986e179e
21120421 <nguyenchicuongk21@gmail.com>
2023-12-20 02:46:32 +0700

    fixconflict

--- WebApplication/Models/MedicineInventoryModel.cs
+++ WebApplication/Models/MedicineInventoryModel.cs
@@ -2,16 +2,16 @@
 
 namespace WebApplication.Models
 {
-	public class MedicineInventoryModel
-	{
-		public int Id { get; set; }
-		[Required]
-		public int MedicineId { get; set; }
-		[Required]
-		public DateOnly ExpiryDate { get; set; }
-		[Required]
-		public int InventoryQuantity { get; set; }
-		[Required]
-		public string? Unit { get; set; }
-	}
+    public class MedicineInventoryModel
+    {
+        public int Id { get; set; }
+        [Required]
+        public int MedicineId { get; set; }
+        [Required]
+        public DateOnly ExpiryDate { get; set; }
+        [Required]
+        public int InventoryQuantity { get; set; }
+        [Required]
+        public string? Unit { get; set; }
+    }
 }
